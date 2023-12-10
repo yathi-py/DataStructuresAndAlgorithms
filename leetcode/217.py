77b4c00d1101ecc0b6d1bd0b79b818db2deb83fa
yathish <iamyathi.py@gmail.com>
2023-12-10 09:51:26 +0530

    217. Contains Duplicate

--- leetcode/217.py
+++ leetcode/217.py
@@ -1,9 +1,19 @@
 # 217. Contains Duplicate
 from typing import List
-
+# Solution : Hashing
+# NOTE: dont use lists for lookups
 class Solution:
 
-    def containsDuplicate(self, nums: List[int]) -> bool:
+    def contains_duplicate_hashing(self, nums: List[int]) -> bool:
+        count_dict = {}
+
+        for idx,val in enumerate(nums):
+            if val in count_dict:
+                return True
+            count_dict[val] = idx
+        return False
+
+    def contains_duplicate_using_sets(self, nums: List[int]) -> bool:
         # sets use hashing for lookups which makes them faster . NOTE: dont use lists for duplicates
         hashset = set(nums) #O(N)
         if len(hashset) <len(nums): #O(1)
