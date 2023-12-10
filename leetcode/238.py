0d23c72d05d3b443b2fa980887a927578831907d
yathish <iamyathi.py@gmail.com>
2023-12-10 09:50:46 +0530

    238. Product of Array Except Self

--- leetcode/238.py
+++ leetcode/238.py
@@ -1,20 +1,26 @@
 # 238. Product of Array Except Self
+
+# Solution: Prefix-Postfix product technique
 # https://leetcode.com/problems/product-of-array-except-self/solutions/3938575/video-visualization-and-explanation-of-o-n-solution/
 
 from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-      ans = [1]* len(nums)
-      for i in range(1,len(nums)):
-          ans[i] = nums[i-1] * ans[i-1]
+        ans = [1]* len(nums)
+        prefix = 1
+        for i in range(0, len(nums)):
+            ans[i] = prefix
+            prefix = prefix*nums[i]
 
-      right = nums[-1]
+        postfix = 1
 
-      for i in range(len(nums)-2, -1, -1):
-          ans[i]= ans[i]*right
-          right = right*nums[i]
-      return ans
+        for i in range(len(nums)-1 ,-1, -1):
+            ans[i] = ans[i]* postfix
+            postfix = postfix * nums[i]
+
+        return ans
+
 
 sol = Solution()
-print(sol.productExceptSelf( nums = [1,2,3,4]))
+print(sol.productExceptSelf( nums = [2,2,3,4]))
 # O(N)
