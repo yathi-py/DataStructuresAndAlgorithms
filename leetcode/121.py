d17e02b60da65b7f0549e0d5f8fc778a2f9aff4a
yathish <iamyathi.py@gmail.com>
2024-01-11 13:22:40 +0530

    121. Best Time to Buy and Sell Stock

--- leetcode/121.py
+++ leetcode/121.py
@@ -1,20 +1,22 @@
-#121. Best Time to Buy and Sell Stock
+# 121. Best Time to Buy and Sell Stock
 # Greedy Approach:
 
 from typing import List
 
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        min_value = prices[0] #O(1)
+        min_value = prices[0]  # O(1)
 
-        for i in range(1,len(prices)): # O(N-1)
-            profit = max(profit, prices[i]-min_value) #O(1)
-            min_value = min(min_value,prices[i])
+        for i in range(1, len(prices)):  # O(N-1)
+            profit = max(profit, prices[i] - min_value)  # O(1)
+            min_value = min(min_value, prices[i])
         return profit
 
+
 sol = Solution()
-print(sol.maxProfit(prices = [7,1,5,3,8]))
+print(sol.maxProfit(prices=[1, 7, 1, 5, 3, 6]))
 
 # time complexity = O(1)+O(N-1)+O(1) ignore constants
 # O(N)
